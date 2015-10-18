0fe6c5d3618e61cc15b560f25c24c0103a2db449
Shelestov Aleksandr <shoroh362@gmail.com>
2015-10-19 02:09:31 +0700

    new approach

--- controllers/tours_controller.js
+++ controllers/tours_controller.js
@@ -1,33 +1,51 @@
-angular.module('tours').controller('ToursController', function ($scope) {
-    // CRUD
-    function getIndex() {
-        $scope.tours        = allTours;
-        $scope.countries    = allCountries;
-        $scope.sortReverse  = false;
+angular.module('tours').controller('ToursController', ToursController);
+
+function ToursController() {
+    "use strict";
+    var vm            = this;
+    vm.tours          = [];
+    vm.countries      = [];
+    vm.sortReverse    = false;
+
+    vm.changeReverse  = changeReverse;
+    vm.imagePath      = imagePath;
+    vm.countryTitle   = countryTitle;
+
+    function init() {
+        getCountries();
+        getTours();
     }
 
-    $scope.changeReverse = function () {
-        "use strict";
-        $scope.sortReverse = !$scope.sortReverse
-    };
+    // CRUD
+    function getTours() {
+        vm.tours = allTours;
+        return vm.tours;
+    }
+
+    function getCountries() {
+        vm.countries = allCountries;
+        return vm.countries;
+    }
+
+    function changeReverse () {
+        vm.sortReverse = !vm.sortReverse
+    }
 
     // Decorator
-    $scope.imagePath = function (tour) {
-        "use strict";
+    function imagePath (tour) {
         if ( tour.image ) {
             return "/assets/images/tours/" + tour.image
         } else {
             return "http://placehold.it/100x100"
         }
-    };
+    }
 
-    $scope.countryTitle = function (tour) {
-        "use strict";
+    function countryTitle (tour) {
         var country = allCountries.filter(function(country) {
             return country.id == tour.country;
         });
         return country? country[0].title : null
-    };
+    }
 
-    getIndex();
-});
+    init();
+}
